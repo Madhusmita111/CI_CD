2e06bd6a1fe31bfdffcee7754966ecfec7c1ae69
Madhusmita Talukdar <tmadhusmita011@gmail.com>
2026-05-01 12:22:45 +0530

    initial commit

---
+++ testApp.py
@@ -0,0 +1,19 @@
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 2) == 3
+    try:
+        divide(6, 0)
+        assert False, "Expected ValueError"
+    except ValueError:
+        pass
+    
